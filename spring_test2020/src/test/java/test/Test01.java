16e6fd9307e8d5742de30f6f30ef3674dedfb504
fengwen <1395006718@qq.com>
2020-11-14 16:23:35 +0800

    2020.11.14-01

--- spring_test2020/src/test/java/test/Test01.java
+++ spring_test2020/src/test/java/test/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("20201114");
+        System.out.println("20201114-01");
     }
 }
